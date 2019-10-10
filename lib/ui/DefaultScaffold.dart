ef4310953fbe30d428c2b60f3fefc6644bf9cb47
TheKingDave <david@langheiter.com>
2019-10-10 19:20:48 +0200

    RM unused import

--- lib/ui/DefaultScaffold.dart
+++ lib/ui/DefaultScaffold.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:lbp/helpers.dart';
 import 'package:lbp/ui/NavDrawer.dart';
 
 class DefaultScaffold extends StatelessWidget {
