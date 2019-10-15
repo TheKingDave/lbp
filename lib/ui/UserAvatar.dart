fc67e225be487f73b9499c0a2ce19dcd34cfe10e
TheKingDave <david@langheiter.com>
2019-10-15 16:33:39 +0200

    Removed api calls

--- lib/ui/UserAvatar.dart
+++ lib/ui/UserAvatar.dart
@@ -1,22 +1,16 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:lbp/api/Api.dart';
 
 class UserAvatar extends StatelessWidget {
   static const defaultImg = "/api/v1/avatars/default-avatar.png";
 
   final String url;
-  final String email;
 
-  UserAvatar(this.url, [this.email]) : super();
+  UserAvatar(this.url) : super();
 
   @override
   Widget build(BuildContext context) {
-    Api api = Api.get();
-
-    String actUrl = url;
-
     return CircleAvatar(
-        backgroundImage: actUrl != null ? NetworkImage(api.getUrl(actUrl)) : null);
+        backgroundImage: url != null ? NetworkImage(url) : null);
   }
 }
