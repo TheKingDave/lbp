4f428e892d036c5e8be586890c674d7344b10ba3
TheKingDave <david@langheiter.com>
2019-10-15 21:23:48 +0200

    Moved from Api to Redux

--- lib/ui/NavDrawer.dart
+++ lib/ui/NavDrawer.dart
@@ -1,10 +1,7 @@
-import 'dart:math';
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
-import 'package:lbp/api/Api.dart';
-import 'package:lbp/api/ApiData.dart';
+import 'package:lbp/redux/selectors/UserSelectors.dart';
 import 'package:lbp/data/LoginData.dart';
 import 'package:lbp/etc/helpers.dart';
 import 'package:lbp/redux/AppState.dart';
@@ -13,55 +10,56 @@ import 'package:lbp/ui/UserAvatar.dart';
 class NavDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return StoreConnector<AppState, _state>(
-      converter: (store) => _state(loginData: store.state.login.data),
+    return StoreConnector<AppState, _State>(
+        converter: (store) => _State(
+            loginData: store.state.login.data,
+            photoUrl: userPhotoSelector(store.state)),
         builder: (context, state) {
-        LoginData loginData = state.loginData;
+          LoginData loginData = state.loginData;
           return Drawer(
               child: ListView(
-                children: <Widget>[
-                  UserAccountsDrawerHeader(
-                      accountEmail: Text(loginData.email),
-                      accountName: Text(loginData.fullName),
-                      currentAccountPicture: UserAvatar(loginData.photo, loginData.email)),
-                  ListTile(
-                    title: Text("Overview"),
-                    onTap: () => cPrint("NAV: Overview"),
-                  ),
-                  Divider(),
-
-                  ListTile(
-                    title: Text("Tuesday"),
-                    onTap: () => cPrint("NAV: Tuesday"),
-                  ),
-                  ListTile(
-                    title: Text("Wendesday"),
-                    onTap: () => cPrint("NAV: Wendesday"),
-                  ),
-                  ListTile(
-                    title: Text("Thursday"),
-                    onTap: () => cPrint("NAV: Thursday"),
-                  ),
-                  Divider(),
-                  ListTile(
-                    leading: Icon(Icons.feedback),
-                    title: Text("Send feedback"),
-                    onTap: () => cPrint("NAV: Issue"),
-                  ),
-                  ListTile(
-                    leading: Icon(Icons.settings),
-                    title: Text("Settings"),
-                    onTap: () => Navigator.pushNamed(context, "/settings"),
-                  ),
-                ],
-              ));
-        }
-    );
+            children: <Widget>[
+              UserAccountsDrawerHeader(
+                  accountName: Text(loginData.fullName),
+                  accountEmail: loginData.email == null ? null : Text(loginData.email),
+                  currentAccountPicture: UserAvatar(state.photoUrl)),
+              ListTile(
+                title: Text("Overview"),
+                onTap: () => cPrint("NAV: Overview"),
+              ),
+              Divider(),
+              ListTile(
+                title: Text("Tuesday"),
+                onTap: () => cPrint("NAV: Tuesday"),
+              ),
+              ListTile(
+                title: Text("Wendesday"),
+                onTap: () => cPrint("NAV: Wendesday"),
+              ),
+              ListTile(
+                title: Text("Thursday"),
+                onTap: () => cPrint("NAV: Thursday"),
+              ),
+              Divider(),
+              ListTile(
+                leading: Icon(Icons.feedback),
+                title: Text("Send feedback"),
+                onTap: () => cPrint("NAV: Issue"),
+              ),
+              ListTile(
+                leading: Icon(Icons.settings),
+                title: Text("Settings"),
+                onTap: () => Navigator.pushNamed(context, "/settings"),
+              ),
+            ],
+          ));
+        });
   }
 }
 
-class _state {
+class _State {
   LoginData loginData;
+  String photoUrl;
 
-  _state({this.loginData});
+  _State({this.loginData, this.photoUrl});
 }
