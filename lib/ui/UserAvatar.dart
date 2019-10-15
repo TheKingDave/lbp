6a354ca0e5bb0d4dd5e44631014f2ad08eaa2ad3
TheKingDave <david@langheiter.com>
2019-10-15 16:32:26 +0200

    Removed Gravatar

--- lib/ui/UserAvatar.dart
+++ lib/ui/UserAvatar.dart
@@ -4,9 +4,6 @@ import 'package:lbp/api/Api.dart';
 
 class UserAvatar extends StatelessWidget {
   static const defaultImg = "/api/v1/avatars/default-avatar.png";
-  static const gravatarUrl = "https://www.gravatar.com/avatar/{md5}/?d={d}";
-  static final calcGravatarUrl = gravatarUrl.replaceAll(
-      new RegExp(r"{d}"), Uri.encodeQueryComponent(Api.get().getUrl(defaultImg)));
 
   final String url;
   final String email;
@@ -18,14 +15,6 @@ class UserAvatar extends StatelessWidget {
     Api api = Api.get();
 
     String actUrl = url;
-
-    /*
-    if (url == defaultImg && email != null) {
-      String md5Hash = md5.convert(utf8.encode(email)).toString();
-      actUrl = calcGravatarUrl.replaceAll("{md5}", md5Hash);
-      cPrint(actUrl);
-    }
-     */
 
     return CircleAvatar(
         backgroundImage: actUrl != null ? NetworkImage(api.getUrl(actUrl)) : null);
