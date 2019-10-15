b2f00a4701f90ff0bc8894d465de1ac1bff2ec46
TheKingDave <david@langheiter.com>
2019-10-15 21:21:34 +0200

    Added config state to app state

--- lib/redux/AppState.dart
+++ lib/redux/AppState.dart
@@ -1,15 +1,20 @@
 import 'package:lbp/data/LoginData.dart';
+import 'package:lbp/redux/states/ConfigState.dart';
 import 'package:lbp/redux/states/FetchState.dart';
 
 class AppState {
   final FetchState<LoginData> login;
   final Exception error;
+  final ConfigState config;
 
   AppState({
     this.login,
     this.error,
+    this.config,
   });
 
-  factory AppState.initial() =>
-      AppState(login: FetchState<LoginData>.initial(), error: null);
+  factory AppState.initial() => AppState(
+      login: FetchState<LoginData>.initial(),
+      error: null,
+      config: ConfigState.initial());
 }
