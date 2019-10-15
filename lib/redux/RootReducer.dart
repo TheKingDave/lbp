fc1023b151e4f6aac2e5545976edc12bfe455478
TheKingDave <david@langheiter.com>
2019-10-15 22:01:47 +0200

    Added subReducers in rootreducer

--- lib/redux/RootReducer.dart
+++ lib/redux/RootReducer.dart
@@ -2,9 +2,10 @@ import 'package:lbp/data/LoginData.dart';
 import 'package:lbp/redux/AppState.dart';
 import 'package:lbp/redux/reducers/ErrorReducers.dart';
 import 'package:lbp/redux/reducers/FetchReducer.dart';
+import 'package:lbp/redux/reducers/UserReducers.dart';
 
 AppState rootReducer(AppState state, action) => AppState(
-  login: fetchReducer<LoginData>(state.login, action),
+  login: fetchReducer<LoginData>(state.login, action, userReducer),
   error: errorReducer(state.error, action),
   config: state.config,
 );
