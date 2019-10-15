a01ff7a2db2930d567e24afd032a0ca1970d5201
TheKingDave <david@langheiter.com>
2019-10-15 22:32:28 +0200

    Added toString methods

--- lib/redux/actions/UserActions.dart
+++ lib/redux/actions/UserActions.dart
@@ -4,10 +4,20 @@ class SetDarkModeAction {
   bool value;
 
   SetDarkModeAction(this.value);
+
+  @override
+  String toString() {
+    return 'SetDarkModeAction{value: $value}';
+  }
 }
 
 class SetLanguageAction {
   Language value;
 
   SetLanguageAction(this.value);
+
+  @override
+  String toString() {
+    return 'SetLanguageAction{value: $value}';
+  }
 }
