9ec8835a33e44576bf9b686938df4f426fac88f6
TheKingDave <david@langheiter.com>
2019-10-15 21:35:00 +0200

    Added darkmode + language actions

---
+++ lib/redux/actions/UserActions.dart
@@ -0,0 +1,13 @@
+import 'package:lbp/api/etc/Languages.dart';
+
+class SetDarkModeAction {
+  bool value;
+
+  SetDarkModeAction(this.value);
+}
+
+class SetLanguageAction {
+  Language value;
+
+  SetLanguageAction(this.value);
+}
