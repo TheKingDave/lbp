6d5c811e213d6be8ad7a2b2aa0ea838f196db694
TheKingDave <david@langheiter.com>
2019-10-23 21:56:23 +0200

    Added ApiGetDataAction

--- lib/redux/actions/ApiActions.dart
+++ lib/redux/actions/ApiActions.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/cupertino.dart';
+import 'package:lbp/data/lessons/Days.dart';
+import 'package:lbp/data/lessons/DaysRequest.dart';
 import 'package:lbp/data/login/LoginRequest.dart';
 import 'package:lbp/data/login/LoginData.dart';
 import 'package:lbp/redux/AppState.dart';
 import 'package:lbp/redux/middleware/ApiMiddleware.dart';
+import 'package:lbp/redux/selectors/UserSelectors.dart';
 import 'package:redux/redux.dart';
 
 import 'FetchAction.dart';
@@ -17,5 +20,13 @@ class ApiLoginAction extends ApiActionWithApiCall<AppState> {
   void call(Store<AppState> store, NextDispatcher next) {
     next(FetchDataAction<LoginData>(
         LoginRequest(username: username, password: password)));
+  }
+}
+
+class ApiGetDataAction extends ApiActionWithApiCall<AppState> {
+  @override
+  void call(Store<AppState> store, next) {
+    next(FetchDataAction<Days>(
+        DaysRequest(sess_key: sessKeySelector(store.state))));
   }
 }
