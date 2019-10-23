df4814de304132e0de4d633d7802674815e23a25
TheKingDave <david@langheiter.com>
2019-10-23 20:52:52 +0200

    Getting data from store

--- lib/screens/OverviewScreen.dart
+++ lib/screens/OverviewScreen.dart
@@ -4,69 +4,63 @@ import 'package:flutter_redux/flutter_redux.dart';
 import 'package:lbp/data/lessons/Lesson.dart';
 import 'package:lbp/data/lessons/Period.dart';
 import 'package:lbp/etc/HexColor.dart';
-import 'package:lbp/etc/helpers.dart';
 import 'package:lbp/redux/AppState.dart';
+import 'package:lbp/ui/Loader.dart';
 
 class OverviewScreen extends StatelessWidget {
-  final data = _LessonOverviewData(
-    color: "#16b8c9",
-    room: "H922",
-    subject: "AI / Data Science",
-    note: "note",
-    period: Period.fromJson({
-      "begin": "2019-10-10T11:20:00.000Z",
-      "end": "2019-10-10T14:50:00.000Z",
-      "name": "afternoon"
-    }),
-  );
-
   @override
   Widget build(BuildContext context) {
     return StoreConnector<AppState, _OverviewData>(
-      converter: (store) => _OverviewData(lessons:
-          List<_LessonOverviewData>.from(store.state.days.data.days.map((d) {
-        Lesson l = d.getSelectedLesson();
-        return _LessonOverviewData(
-          color: l?.color ?? '#000000',
-          period: d.period,
-          note: d.note.isEmpty ? "Note" : d.note,
-          room: l?.room ?? 'Raum',
-          subject: l?.subject ?? 'Gegenstand',
-        );
-      }))),
+      converter: (store) => _OverviewData(
+          loading: store.state.days.loading,
+          lessons: store.state.days.data == null
+              ? List<_LessonOverviewData>()
+              : List<_LessonOverviewData>.from(
+                  store.state.days.data.days.map((d) {
+                  Lesson l = d.getSelectedLesson();
+                  return _LessonOverviewData(
+                    color: l?.color,
+                    period: d.period,
+                    note: d.note,
+                    room: l?.room,
+                    subject: l?.subject,
+                  );
+                }))),
       builder: (context, model) {
-        return Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: model.lessons.map((l) => _LessonOverview(l)).toList(),
+        return Loader(
+          loading: model.loading,
+          build: (context) {
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: model.lessons.map((l) => _LessonOverview(l)).toList(),
+            );
+          },
         );
       },
     );
-    /*return Column(
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: <Widget>[
-        _LessonOverview(data),
-        _LessonOverview(data),
-        _LessonOverview(data),
-      ],
-    );*/
   }
 }
 
 class _OverviewData {
   List<_LessonOverviewData> lessons;
+  bool loading;
 
-  _OverviewData({this.lessons});
+  _OverviewData({this.lessons, this.loading});
 }
 
 class _LessonOverviewData {
-  Period period;
-  String color;
-  String subject;
-  String room;
-  String note;
+  final Period period;
+  final String color;
+  final String subject;
+  final String room;
+  final String note;
 
   _LessonOverviewData(
-      {this.period, this.color, this.subject, this.room, this.note});
+      {this.period, String color, String subject, String room, String note})
+      : this.color = color ?? "#000000",
+        this.subject = subject ?? "Gegenstand",
+        this.room = room ?? "Raum",
+        this.note = note.isEmpty ? "Note" : note;
 }
 
 class _LessonOverview extends StatelessWidget {
