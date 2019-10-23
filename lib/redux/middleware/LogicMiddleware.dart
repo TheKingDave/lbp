04d948082974e17033d279ebb2db58cdd3bae4a7
TheKingDave <david@langheiter.com>
2019-10-23 21:48:10 +0200

    Made logic middleware simpler + working

--- lib/redux/middleware/LogicMiddleware.dart
+++ lib/redux/middleware/LogicMiddleware.dart
@@ -1,28 +1,21 @@
+import 'package:lbp/etc/helpers.dart';
 import 'package:redux/redux.dart';
 
 abstract class Logic<State> {
-  /// Defines if the other logic should be called or the action passed on
-  bool exclusive;
 
-  Logic({this.exclusive = false});
 
-  /// Return `true` if logic was executed (Used for exclusive)
-  bool call(Store<State> store, NextDispatcher next, dynamic action);
+  void call(Store<State> store, NextDispatcher next, dynamic action);
 }
 
 class TypedLogic<State, Action> extends Logic<State> {
   final void Function(Store<State> store, NextDispatcher next, Action action)
       exec;
 
-  TypedLogic(this.exec, {exclusive}) : super(exclusive: exclusive);
+  TypedLogic(this.exec);
 
   @override
-  bool call(Store<State> store, NextDispatcher next, action) {
-    if (action is Action) {
-      exec(store, next, action);
-      return true;
-    }
-    return false;
+  void call(Store<State> store, NextDispatcher next, action) {
+    if (action is Action) exec(store, next, action);
   }
 }
 
@@ -33,11 +26,9 @@ class LogicMiddleware<State> extends MiddlewareClass<State> {
 
   @override
   void call(Store<State> store, action, next) {
-    for (Logic l in logic) {
-      if (l.call(store, next, action) && l.exclusive) {
-        return;
-      }
-    }
     next(action);
+    for (Logic l in logic) {
+      l.call(store, next, action);
+    }
   }
 }
