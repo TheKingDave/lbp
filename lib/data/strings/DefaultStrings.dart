fcda9afa6745e722b4a551eea828b8370c2ab545
TheKingDave <david@langheiter.com>
2019-10-24 09:08:35 +0200

    Added default strings for session strings

--- lib/data/strings/DefaultStrings.dart
+++ lib/data/strings/DefaultStrings.dart
@@ -1,3 +1,5 @@
+import 'package:lbp/data/strings/LessonStrings.dart';
+
 import 'Strings.dart';
 
 class DefaultStrings {
@@ -20,5 +22,39 @@ class DefaultStrings {
     };
 
     Strings.overrideMany(strings);
+
+    Strings.lessons = LessonStrings.fromJson({
+      "subject": ["Subject", "SUBJ"],
+      "ger": ["German", "D"],
+      "am": ["Mathematics", "AM"],
+      "am2": ["Mathematics", "AM"],
+      "eng": ["English", "E"],
+      "sew": ["Software Development", "SEW"],
+      "sew2": ["Software Development", "SEW"],
+      "nwtk": ["Networking Technology", "NWTK"],
+      "syte": ["System Technology Electronics", "SYTE"],
+      "syteahit": ["System Technology Electronics (Only AHIT)", "SYTE"],
+      "sytebhit": ["System Technology Electronics (Only BHIT)", "SYTE"],
+      "syti": ["System Technology Computer Science ", "SYTI"],
+      "sytit": ["System Technology Computer Science (Theory)", "SYTI"],
+      "sytit2": ["System Technology Computer Science (Theory)", "SYTI"],
+      "syt": ["System Technology", "SYT"],
+      "syt2": ["System Technology", "SYT"],
+      "syt3": ["System Technology", "SYT"],
+      "itsi": ["IT Security", "ITSI"],
+      "ai": ["AI / Data Science", "AI"],
+      "insy": ["Information Systems", "INSY"],
+      "itp": ["Projectmanagment", "ITP"],
+      "nw": ["Natural Sciences", "NW"],
+      "medt": ["Media technology", "MEDT"],
+      "medt3d": ["3D & Game Development", "3DG"],
+      "medtmm": ["Multimedia", "MM"],
+      "medtweb": ["Web Development", "WE"],
+      "es1": ["Energiesysteme", "ES"],
+      "aiit": ["Informatik", "AIIT"],
+      "aiit2": ["Informatik", "AIIT"],
+      "amhet": ["Mathematics (ONLY HET!!!)", "AM (HET)"],
+      "amhit": ["Mathematics (ONLY HIT!!!)", "AM (HIT)"]
+    });
   }
 }
