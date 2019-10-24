ab0e068453e011629f7c5319969e0164d683c49d
TheKingDave <david@langheiter.com>
2019-10-24 21:30:09 +0200

    Mapped logout to init action

--- lib/logic/Logics.dart
+++ lib/logic/Logics.dart
@@ -2,7 +2,9 @@ import 'package:lbp/data/login/LoginData.dart';
 import 'package:lbp/redux/AppState.dart';
 import 'package:lbp/redux/actions/ApiActions.dart';
 import 'package:lbp/redux/actions/FetchAction.dart';
+import 'package:lbp/redux/actions/InitAction.dart';
 import 'package:lbp/redux/actions/RouteActions.dart';
+import 'package:lbp/redux/actions/UserActions.dart';
 import 'package:lbp/redux/middleware/LogicMiddleware.dart';
 import 'package:redux/redux.dart';
 
@@ -10,10 +12,15 @@ import '../RouteNames.dart';
 
 final List<Logic> logics = [
   TypedLogic<AppState, FetchActionSuccess<LoginData>>(_loginSuccessLogic),
+  TypedLogic<AppState, LogoutAction>(_logout),
 ];
 
 void _loginSuccessLogic(Store<AppState> store, NextDispatcher next,
     FetchActionSuccess<LoginData> action) {
   store.dispatch(ApiGetDataAction());
   next(NavigateReplaceAction(RouteNames.overview));
+}
+
+void _logout(Store<AppState> store, NextDispatcher next, action) {
+  next(InitAction());
 }
