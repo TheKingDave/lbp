890a33ac896e54389ff52f99c9a41b6f35b85916
TheKingDave <david@langheiter.com>
2019-10-24 21:28:29 +0200

    Added InitAction with posibility to reset the state

--- lib/redux/RootReducer.dart
+++ lib/redux/RootReducer.dart
@@ -1,13 +1,20 @@
 import 'package:lbp/data/lessons/Days.dart';
 import 'package:lbp/data/login/LoginData.dart';
 import 'package:lbp/redux/AppState.dart';
+import 'package:lbp/redux/actions/InitAction.dart';
 import 'package:lbp/redux/reducers/ErrorReducers.dart';
 import 'package:lbp/redux/reducers/FetchReducer.dart';
 import 'package:lbp/redux/reducers/UserReducers.dart';
 
-AppState rootReducer(AppState state, action) => AppState(
-  login: fetchReducer<LoginData>(state.login, action, userReducer),
-  days: fetchReducer<Days>(state.days, action),
-  error: errorReducer(state.error, action),
-  config: state.config,
-);
+AppState rootReducer(AppState state, action) {
+  if(action is InitAction) {
+    return AppState.initial();
+  } else {
+    return AppState(
+      login: fetchReducer<LoginData>(state.login, action, userReducer),
+      days: fetchReducer<Days>(state.days, action),
+      error: errorReducer(state.error, action),
+      config: state.config,
+    );
+  }
+}
