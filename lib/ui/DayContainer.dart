0420da4ba2719cf43be680a2390f7422b40f12b8
TheKingDave <david@langheiter.com>
2019-10-25 15:26:37 +0200

    Added DayContainer for reasons of initilization for DayScaffold

---
+++ lib/ui/DayContainer.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_redux/flutter_redux.dart';
+import 'package:lbp/data/lessons/Class.dart';
+import 'package:lbp/logic/DayRouteData.dart';
+import 'package:lbp/redux/AppState.dart';
+import 'package:lbp/redux/selectors/DaysSelectors.dart';
+import 'package:lbp/ui/DayScaffold.dart';
+import 'package:redux/redux.dart';
+
+class DayContainer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return StoreBuilder<AppState>(
+      builder: (BuildContext context, Store<AppState> store) {
+        DayRouteData data = ModalRoute.of(context).settings.arguments;
+        List<Class> classes = classesOfDaySelector(store.state, data.day);
+
+        return DayScaffold(
+          classes: classes,
+          data: data,
+        );
+      },
+    );
+  }
+
+
+
+}
