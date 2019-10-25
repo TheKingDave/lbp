7705f09736ba59764be608921f70245f039194ea
TheKingDave <david@langheiter.com>
2019-10-25 15:07:52 +0200

    Added classes for weekday selector

--- lib/redux/selectors/DaysSelectors.dart
+++ lib/redux/selectors/DaysSelectors.dart
@@ -1,7 +1,18 @@
+import 'package:lbp/data/lessons/Class.dart';
 import 'package:lbp/data/lessons/Days.dart';
 
 import '../AppState.dart';
 
 List<Day> daysListSelector(AppState state) {
   return state.days.data?.days;
+}
+
+List<Class> classesOfDaySelector(AppState state, int weekDay) {
+  for(Day d in daysListSelector(state)) {
+    if(d.getWeekDay() == weekDay) {
+      return d.classes;
+    }
+  }
+
+  return null;
 }
