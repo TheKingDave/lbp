14c0370072fcf5a1a439b478165196f5900e4aa6
TheKingDave <david@langheiter.com>
2019-10-27 12:37:22 +0100

    Added SetDataRequest

---
+++ lib/data/SetDataRequest.dart
@@ -0,0 +1,29 @@
+import 'dart:convert';
+
+import 'package:lbp/api/ApiRequestWithKey.dart';
+
+class SetDataRequest extends ApiRequestWithKey {
+  DateTime begin;
+  DateTime end;
+  String subject;
+
+  Map<String, dynamic> toJson() {
+    return {
+      "begin": begin.toIso8601String(),
+      "end": end.toIso8601String(),
+      "subject": subject,
+      "sess_key": sessKey,
+    };
+  }
+
+  @override
+  String getBody() {
+    return jsonEncode(this.toJson());
+  }
+
+  @override
+  String getEndpoint() {
+    return "getData";
+  }
+
+}
