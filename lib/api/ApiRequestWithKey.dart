b6ca50684c1feae08aa462a7a834f8a0d51f61b2
TheKingDave <david@langheiter.com>
2019-10-27 12:37:11 +0100

    Added Api reqeust with session key

---
+++ lib/api/ApiRequestWithKey.dart
@@ -0,0 +1,21 @@
+import 'dart:convert';
+
+import 'package:lbp/redux/AppState.dart';
+import 'package:redux/redux.dart';
+
+abstract class ApiRequestWithKey {
+  String sessKey;
+
+  Map<String, String> getHeaders() => const {"content-type": "application/json"};
+  Encoding getEncoding() => null;
+  String getEndpoint();
+  String getBody();
+
+  void onSuccess(Store<AppState> store, NextDispatcher next) {}
+  void onFailure(Store<AppState> store, NextDispatcher next) {}
+
+  void setSesKey(String sessKey) {
+    this.sessKey = sessKey;
+  }
+
+}
