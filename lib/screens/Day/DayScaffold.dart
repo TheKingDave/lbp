a0c27ad4a102f01b071c573970d3afa22f6994ff
TheKingDave <david@langheiter.com>
2019-10-31 20:39:52 +0100

    Added ErrorNotifier to DayScaffold

--- lib/screens/Day/DayScaffold.dart
+++ lib/screens/Day/DayScaffold.dart
@@ -4,6 +4,7 @@ import 'package:lbp/data/lessons/Class.dart';
 import 'package:lbp/data/strings/Strings.dart';
 import 'package:lbp/logic/DayRouteData.dart';
 import 'package:lbp/screens/Day/DayScreen.dart';
+import 'package:lbp/ui/ErrorNotifier.dart';
 import 'package:lbp/ui/NavDrawer.dart';
 
 class DayScaffold extends StatefulWidget {
@@ -49,12 +50,14 @@ class _DayScaffoldState extends State<DayScaffold>
             isScrollable: classes.length > 2,
           ),
         ),
-        body: TabBarView(
-          controller: _tabController,
-          children: List.from(classes.map((c) => DayScreen(
-            weekDay: data.day,
-            $class: index++,
-          ))),
+        body: ErrorNotifier(
+          child: TabBarView(
+            controller: _tabController,
+            children: List.from(classes.map((c) => DayScreen(
+              weekDay: data.day,
+              $class: index++,
+            ))),
+          ),
         ));
   }
 }
