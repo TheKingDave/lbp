46b96efa213a8863c9a914976749c2602c797654
TheKingDave <david@langheiter.com>
2019-11-10 13:38:28 +0100

    Removed 'Api' useage

--- lib/redux/selectors/UserSelectors.dart
+++ lib/redux/selectors/UserSelectors.dart
@@ -1,13 +1,6 @@
-import 'package:lbp/data/Language.dart';
+import 'package:lbp/etc/Constants.dart';
 import 'package:lbp/redux/AppState.dart';
-import 'package:lbp/settings/Api.dart';
 
 String userPhotoSelector(AppState state) => state.login.data.photo == null
     ? null
-    : Api.getUrl(state.login.data.photo);
-
-String urlSelector(AppState state, [extension = ""]) =>
-    Api.getUrl(extension);
-
-String apiUrlSelector(AppState state, [extension = ""]) =>
-    Api.getApiUrl(extension);
+    : "${Constants.api_web_url}${state.login.data.photo}";
