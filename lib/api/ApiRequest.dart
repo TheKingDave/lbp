bbc3795e042e51ee0a1b755787cd21a35d634a5e
TheKingDave <david@langheiter.com>
2019-11-10 13:28:07 +0100

    Added getUrl method

--- lib/api/ApiRequest.dart
+++ lib/api/ApiRequest.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:lbp/etc/Constants.dart';
 import 'package:lbp/redux/AppState.dart';
 import 'package:redux/redux.dart';
 
@@ -7,6 +8,11 @@ abstract class ApiRequest {
 
   Map<String, String> getHeaders() => const {"content-type": "application/json"};
   Encoding getEncoding() => null;
+
+  String getUrl() {
+    return Constants.api_web_url + "/" + Constants.api_url + getEndpoint();
+  }
+
   String getEndpoint();
   String getBody();
 
