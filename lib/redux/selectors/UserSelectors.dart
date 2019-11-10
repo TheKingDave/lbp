ad07e787c0918ada51c594f75b14a4d8dd22bb5b
TheKingDave <david@langheiter.com>
2019-11-10 14:01:42 +0100

    Added emailSelector

--- lib/redux/selectors/UserSelectors.dart
+++ lib/redux/selectors/UserSelectors.dart
@@ -4,3 +4,6 @@ import 'package:lbp/redux/AppState.dart';
 String userPhotoSelector(AppState state) => state.login.data.photo == null
     ? null
     : "${Constants.api_web_url}${state.login.data.photo}";
+
+String emailSelector(AppState state) =>
+    state.login.data?.email ?? state.login.data?.fullName ?? 'UNKNOWN';
